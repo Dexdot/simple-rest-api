6e2f47f28e64cba8457303376950d9ab8c1b2db0
Dexdot <dexdotapb@gmail.com>
2019-03-03 21:28:16 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import config from './config'
+import cors from 'cors'
+import { signup, signin, protect } from './utils/auth'
+import { connect } from './utils/db'
+import userRouter from './resources/user/user.router'
+import itemRouter from './resources/item/item.router'
+import listRouter from './resources/list/list.router'
+
+// App
+export const app = express()
+
+// Middlewares
+app.disable('x-powered-by')
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+// Auth
+app.post('/signup', signup)
+app.post('/signin', signin)
+
+// API
+app.use('/api', protect)
+app.use('/api/user', userRouter)
+app.use('/api/item', itemRouter)
+app.use('/api/list', listRouter)
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
